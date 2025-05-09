91c6aaddbf493770b695667bb69d45a5759a6196
Remiiiiii <developer@remyouji.com>
2025-05-09 11:45:46 -0400

    resolving deployment issue

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Prisma } from "../generated/prisma";
+import { PrismaClient, Prisma } from "@prisma/client";
 import { Decimal } from "@prisma/client/runtime/library";
 
 const prisma = new PrismaClient();
